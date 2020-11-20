f7f2e0126fd965feb4bff123d6209be200018657
Parteek <sparteek9@gmail.com>
2020-11-20 21:10:27 +0530

    Model Created

--- Softwareproject-master/bookswap/admin.py
+++ Softwareproject-master/bookswap/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib.auth.models import User
+from .models import Listing
+
+admin.site.register(Listing)
--- Softwareproject-master/bookswap/models.py
+++ Softwareproject-master/bookswap/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.base import ModelState
+from django.db.models.deletion import CASCADE
+from django.contrib.auth import get_user_model
+class Listing(models.Model):
+    user=models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    first_name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+    have=models.CharField(max_length=120)
+    want=models.CharField(max_length=120)
+    img=models.ImageField(upload_to="static/",default='static/bookswap/book.svg')
+    
+    def __str__(self):
+        return (f'{self.first_name}  Has:{self.have}  Want:{self.want}')  
